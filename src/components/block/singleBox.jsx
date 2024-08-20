26920d50ab8801e37e96b7991d0bd0aec6603e60
ChandanVasu <chandanyadavparas@gmail.com>
2024-08-21 00:21:05 +0530

    fix error gap

--- src/components/block/singleBox.jsx
+++ src/components/block/singleBox.jsx
@@ -4,7 +4,7 @@ import { Skeleton } from "@nextui-org/react";
 import Link from 'next/link';
 
 const renderSkeleton = () => (
-    <div className="flex flex-col justify-center items-center gap-4 mb-1 p-4">
+    <div className="flex flex-col justify-center items-center gap-4">
         <Skeleton className="h-28 w-52 rounded-md mb-2" />
         <Skeleton className="h-5 w-52 rounded-md" />
     </div>
@@ -42,9 +42,9 @@ const SingleBox = () => {
                 </div>
 
                 {loading ? (
-                    <div className="grid grid-cols-3">
+                    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 gap-4">
                         {Array(6).fill(null).map((_, index) => (
-                            <div className='flex flex-col justify-center items-center mb-4' key={index}>
+                            <div className='flex flex-col justify-center items-center' key={index}>
                                 {renderSkeleton()}
                             </div>
                         ))}
@@ -53,14 +53,14 @@ const SingleBox = () => {
                     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 gap-4 mt-4">
                         {typeData.map((item, index) => (
                             <div className='flex flex-col justify-center items-center gap-2 mb-4' key={index}>
-                                <Link href={`/cars/type/${item.type}`}>  <img src={item.image} alt={item.type} className='w-52 h-28 rounded-md object-cover shadow-md p-1' /></Link>
+                                <Link href={`/cars/type${item.type}`}>  <img src={item.image} alt={item.type} className='w-52 h-28 rounded-md object-cover shadow-md p-1' /></Link>
                                 <h1 className='text-base'>{item.type}</h1>
                             </div>
                         ))}
                     </div>
                 )}
             </div>
-            <div className='w-full mt-4 md:mt-0'>
+            <div className='w-full mt-1 md:mt-0'>
                 <EmiCalculator />
             </div>
         </div>
