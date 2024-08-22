4c3bfd52a5d01e6e15ee6539e90b016a6c680d4d
ChandanVasu <chandanyadavparas@gmail.com>
2024-08-22 18:13:47 +0530

    fix

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,15 +11,15 @@ export default function App() {
     <div>
       <div className="top-main relative">
         <img
-          className="w-full h-500 object-fill object-top relative"
-          src="/duplo24.jpg"
+          className="w-full h-500 object-cover object-center relative"
+          src="/bg.svg"
           alt="Showroom" />
         <div className="h-full w-full absolute top-0 left-0 flex justify-between items-center px-5 md:px-20 flex-col md:flex-row pt-4">
           <div className="mw-1/2 pr-0 md:pr-10 text-center md:text-start mt-0 md:mt-3">
-            <h1 className="text-4xl md:text-5xl font-bold mb-6 bg-gradient-to-r from-purple-900 via-blue-800 to-black bg-clip-text text-transparent leading-tight">
+            <h1 className="text-4xl md:text-5xl font-bold mb-6 text-white">
               Experience Excellence with CarsX
             </h1>
-            <p className="text-xl mb-2 md:mb-8 bg-gradient-to-r from-purple-900 via-blue-800 to-black bg-clip-text text-transparent leading-relaxed">
+            <p className="text-xl mb-2 md:mb-8 text-white">
               Unlock unbeatable offers on the newest models and take the wheel of your dream car today.
             </p>
           </div>
